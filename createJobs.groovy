af62e985d8ca0c35955f750454ed2bbe8308682f
MD HASAN <hasan.mmd28@gmail.com>
2021-12-11 04:46:28 -0500

    theme-park-jab adde

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm{
+            scm{
+                git{
+                    remote{
+                        url 'https://github.com/mdmhasan307/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+
+        }
+    }
+}
